b00cb740ef70145c323d45f088d544957de72254
jennie <aliyajenny14@gmail.com>
2025-03-24 19:38:15 +0700

    adding palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    corePlugins: {
+      preflight: true,
+    },
     content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {
         colors: {
           brown: "#8d6649",
+        },
+        textColor: {
+          brown: "#493628",
         },
       },
     },
