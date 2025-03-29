0fba69d5eeaec9400843cd40ad3792ef312fa6f4
jennie <aliyajenny14@gmail.com>
2025-03-29 15:32:58 +0700

    customize color

--- frontend/app/cart/page.tsx
+++ frontend/app/cart/page.tsx
@@ -73,7 +73,7 @@ export default function page() {
             </div>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 min-[550px]:gap-6 border-t border-gray-200 py-6">
+        {/* <div className="grid grid-cols-1 lg:grid-cols-2 min-[550px]:gap-6 border-t border-gray-200 py-6">
             <div
                 className="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto">
                 <div className="img-box"><img src="https://pagedone.io/asset/uploads/1701162866.png" alt="perfume bottle image" className="xl:w-[140px] !rounded-xl object-cover"/></div>
@@ -178,8 +178,9 @@ export default function page() {
                     className="text-indigo-600 font-bold text-2xl leading-9 w-full max-w-[176px] text-center">
                     $120.00</h6>
             </div>
-        </div>
-        <div className="bg-[#f8f5f2] !rounded-xl p-6 w-full mb-8 max-lg:max-w-xl max-lg:mx-auto">
+        </div> */}
+
+        <div className="bg-[#f8f5f2] !rounded-xl p-6 w-full mb-5 max-lg:max-w-xl max-lg:mx-auto">
             <div className="flex items-center justify-between w-full mb-6">
                 <p className="font-normal text-xl leading-8 text-[#705C53]">Sub Total</p>
                 <h6 className="font-semibold text-xl leading-8 text-[#493628]">$360.00</h6>
@@ -195,15 +196,15 @@ export default function page() {
         </div>
         <div className="flex items-center flex-col sm:flex-row justify-center gap-3 mt-8">
             <button
-                className="!rounded-full py-4 w-full max-w-[280px]  flex items-center bg-indigo-50 justify-center transition-all duration-500 hover:bg-indigo-100">
-                <span className="px-2 font-semibold text-lg leading-8 text-indigo-600">Add Coupon Code</span>
+                className="!rounded-full py-4 w-full max-w-[280px] flex items-center bg-[#f8f5f2] justify-center transition-all duration-500 hover:bg-[#f4ebe1]">
+                <span className="px-2 font-semibold text-lg leading-8 text-[#493628]">Add Coupon Code</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-                    <path d="M8.25324 5.49609L13.7535 10.9963L8.25 16.4998" stroke="#4F46E5" strokeWidth="1.6"
+                    <path d="M8.25324 5.49609L13.7535 10.9963L8.25 16.4998" stroke="#493628" strokeWidth="1.6"
                         strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
             </button>
             <button
-                className="!rounded-full w-full max-w-[280px] py-4 text-center justify-center items-center bg-indigo-600 font-semibold text-lg text-white flex transition-all duration-500 hover:bg-indigo-700">Continue
+                className="!rounded-full w-full max-w-[280px] py-4 text-center justify-center items-center bg-[#493628] font-semibold text-lg text-white flex transition-all duration-500 hover:bg-[#705C53]">Continue
                 to Payment
                 <svg className="ml-2" xmlns="http://www.w3.org/2000/svg" width="23" height="22" viewBox="0 0 23 22"
                     fill="none">
