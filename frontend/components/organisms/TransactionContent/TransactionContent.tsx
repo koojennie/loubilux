e282944534f54eb29bd71fc63bc2ff1484265a6e
jennie <aliyajenny14@gmail.com>
2025-06-03 10:05:58 +0700

    adding catalog ui

--- frontend/components/organisms/TransactionContent/TransactionContent.tsx
+++ frontend/components/organisms/TransactionContent/TransactionContent.tsx
@@ -11,7 +11,7 @@ export default function TransactionContent() {
         <div className="mb-30">
           <p className="text-lg color-palette-2 mb-12">You’ve spent</p>
           <h3 className="text-5xl fw-medium color-palette-1">
-            Rp 4.518.000.500
+            {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(Number(3190000))}
           </h3>
         </div>
         <div className="row mt-30 mb-20">
